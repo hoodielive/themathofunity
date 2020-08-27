9dea8f897858c9cdf6bcce1fbf5314baa5912606
Lucid Developer <hoodielive@gmail.com>
2020-08-27 00:15:30 -0400

    Exam prep

---
+++ Prototyping/Assets/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUp : MonoBehaviour
+{
+    /* Transform:
+    1. Sets position, rotation and scale of the GameObject.
+    2. This is the only component 'required' of 'every' GameObject.
+    */
+}
